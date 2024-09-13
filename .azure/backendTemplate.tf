8b60085399287adcf1c6a922070099732ba01856
qfai <404630507@qq.com>
2024-09-13 15:05:28 +0800

    Create files

---
+++ .azure/backendTemplate.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "zilutest"
+    storage_account_name = "ziluiacbackend"
+    container_name       = "tfbackend"
+    key                  = "{{.GroupName}}.tfstate"
+    use_azuread_auth     = true
+    subscription_id      = "586c20df-c465-4f10-8673-65aa4859e7ca"
+  }
+}
